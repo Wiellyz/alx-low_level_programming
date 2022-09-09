449b1e2e39e2257115e277b1bbb9fc9fa080b711
Williams Kikelomo <williamskikelomom@gmail.com>
2022-09-08 18:47:16 -0700

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,11 +14,11 @@ long li;
 long long lli;
 float f;
 
-printf("Size of a char: %d\n", sizeof(c));
-printf("Size of an int: %d\n", sizeof(i));
-printf("Size of a kong: %d\n", sizeof(li));
-printf("Size of a long long: %d\n", sizeof(lli));
-printf("Size of a float %d\n", sizeof(f));
+printf("Size of a char: %d byte(s)\n", sizeof(c));
+printf("Size of an int: %d byte(s)\n", sizeof(i));
+printf("Size of a kong: %d byte(s)\n", sizeof(li));
+printf("Size of a long long: %d byte(s)\n", sizeof(lli));
+printf("Size of a float %d byte(s)\n", sizeof(f));
 
 return (0);
 }
