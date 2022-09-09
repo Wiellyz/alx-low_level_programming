247b8e6e62d3d4e54cbbbf64f257aa36947b421f
Williams Kikelomo <williamskikelomom@gmail.com>
2022-09-08 18:13:10 -0700

    Completed optional tasks

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int i;
+char c;
+long li;
+long long lli;
+float f;
+
+printf("Size of a char: %d\n", sizeof(c));
+printf("Size of an int: %d\n", sizeof(i));
+printf("Size of a kong: %d\n", sizeof(li));
+printf("Size of a long long: %d\n", sizeof(lli));
+printf("Size of a float %d\n", sizeof(f));
+
+return (0);
+}
