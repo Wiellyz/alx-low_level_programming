8895d2064f4f85c707e6a7480b3dfe8e33b0d113
Williams Kikelomo <williamskikelomom@gmail.com>
2022-09-13 12:15:17 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - Function to print abc
+ * Description: Print the alphabets in lower case
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
