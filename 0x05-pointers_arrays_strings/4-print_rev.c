4f03a7e23def8bbdd99eed52540c3b6a5548b958
Williams Kikelomo <williamskikelomom@gmail.com>
2022-09-20 15:01:15 -0700

    Task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include "2-strlen.c"
+/**
+ * print_rev - Function that reverses the string
+ * @s: String to print
+ * Return: 0
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = _strlen(s) - 1; i >= 0; i++)
+	{
+		_putchar(*(s + i));
+	}
+	_putchar('\n');
+}
