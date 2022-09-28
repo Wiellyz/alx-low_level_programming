51251c0a27085b6c39b08db3eafee87e3c49ee3f
Williams Kikelomo <williamskikelomom@gmail.com>
2022-09-27 18:39:43 -0700

    Task 8

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * set_string - function that sets a string
+ * @s: string to set
+ * @to: value to set to
+ * Return: result
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
